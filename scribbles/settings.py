b7f2362929f7b0f6a418043f8e9aeaee4e575e4d
MillieNakiganda <millienakiganda@gmail.com>
2020-03-16 15:45:09 +0300

    feature setup: dotnev setup dotnev finish #171110040

--- scribbles/settings.py
+++ scribbles/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't09!*#-0cvqs69mm*%x3=)(l^h5+wjfhdi$q)mis*v^e9n-woj'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +78,13 @@ WSGI_APPLICATION = 'scribbles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.env("NAME"),
+        'USER' : os.env("USER"),
+        'PASSWORD' : os.env("PASSWORD"),
+        'HOST' : os.env("HOST"),
+        'PORT' : os.env("PORT")
+
     }
 }
 
